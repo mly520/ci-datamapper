abf75b4e45c4f352c9061adcd1563e7dbe8e84fc
MaReiiku <mly520@gmail.com>
2013-04-10 13:20:56 +0900

    日本語

--- application/language/japanese/datamapper_lang.php
+++ application/language/japanese/datamapper_lang.php
@@ -1,25 +1,27 @@
 <?php
 
-$lang['alpha_dash_dot']		= 'The %s field may only contain alpha-numeric characters, underscores, dashes, and full stops.';
-$lang['alpha_slash_dot']	= 'The %s field may only contain alpha-numeric characters, underscores, dashes, slashes, and full stops.';
-$lang['min_date']			= 'The %s field must be at least %s.';
-$lang['max_date']			= 'The %s field can not exceed %s.';
-$lang['min_size']			= 'The %s field must be at least %s.';
-$lang['max_size']			= 'The %s field can not exceed %s.';
-$lang['transaction']		= 'The %s failed to %s.';
-$lang['unique'] 			= 'The %s you supplied is already taken.';
-$lang['unique_pair'] 		= 'The combination of %s and %s you supplied is already taken.';
-$lang['valid_date']			= 'The %s field must contain a valid date.';
-$lang['valid_date_group']	= 'The %2$s fields must contain a valid date.';
-$lang['valid_match']		= 'The %s field may only be %s.';
+$lang['alpha_dash_dot']	= '%s フィールドには英数字、アンダースコア、ダッシュ、フルストップ以外を入力できません.';
+$lang['alpha_slash_dot']	= '%s フィールドには英数字、アンダースコア、ダッシュ、スラッシュ、フルストップ以外を入力できません.';
+$lang['min_date']	= '%s フィールドは最低 %s 以上でなければなりません.';
+$lang['max_date']	= '%s フィールドは %s を超えてはいけません.';
+$lang['min_size']	= '%s フィールドは 最低 %s 文字以上でなければなりません.';
+$lang['max_size']	= '%s フィールドは %s 文字を超えてはいけません.';
+$lang['transaction']	= '%s は %s　を失敗しました.';
+$lang['unique'] = '%s はすでに使用されています.';
+$lang['unique_pair'] = '%s と %s の組合せはすでに使用されています.';
+$lang['valid_date']	= '%s フィールドは有効な日付型でなくてはいけません。';
+$lang['valid_date_group']	= '%2$s フィールドは有効な日付型でなくてはいけません。';
+$lang['valid_match']	= '%s フィールドには　%s　以外を入力できません.';
 
-$lang['related_required']	= 'The %s relationship is required.';
-$lang['related_min_size']	= 'The %s relationship must be at least %s.';
-$lang['related_max_size']	= 'The %s relationship can not exceed %s.';
+$lang['related_required']	= '%s リレーションシップは必須です.';
+$lang['related_min_size']	= '%s リレーションシップは 最低 %s 文字以上でなければなりません.';
+$lang['related_max_size']	= '%s リレーションシップは %s 文字を超えてはいけません.';
 
-$lang['dm_save_rel_failed']	= 'The %s relationship is not properly defined.';
-$lang['dm_save_rel_nothis']	= 'Unable to save the %s relationship: This object is not saved.';
-$lang['dm_save_rel_noobj']	= 'Unable to save the %s relationship: The related object was not saved.';
+$lang['dm_save_rel_failed']	= '%s リレーションシップは適切に定義されていません.';
+$lang['dm_save_rel_nothis']	= '%s リレーションシップを保存できませんでした: このオブジェクトは保存されていません.';
+$lang['dm_save_rel_noobj']	= '%s リレーションシップを保存できませんでした: 関連するオブジェクトは保存されませんでした.';
+
+
 
 /* End of file datamapper_lang.php */
 /* Location: ./application/language/english/datamapper_lang.php */
